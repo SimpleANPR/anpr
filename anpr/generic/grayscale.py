26598313e209a0805cb4872ce80695a6f1e6035f
Moésio Filho <moesio.f@gmail.com>
2023-08-10 16:54:00 -0300

    feat: add gray scale conversion

---
+++ anpr/generic/grayscale.py
@@ -0,0 +1,17 @@
+"""Filtragem através do filtro
+média.
+"""
+from __future__ import annotations
+
+import cv2
+import numpy as np
+
+from anpr.core import ImageProcessor
+
+
+class GrayScale(ImageProcessor):
+    def __init__(self, conversion: int = cv2.COLOR_BGR2GRAY):
+        self._conversion = conversion
+
+    def process(self, image: np.ndarray) -> np.ndarray:
+        return cv2.cvtColor(image, self._conversion)
