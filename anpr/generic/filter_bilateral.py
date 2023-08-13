d7698502ee59443ff136849793f4657dbb8775d9
mrjohnnus <jpedrofa15@gmail.com>
2023-08-12 21:20:11 -0300

    fix: class name correction

--- anpr/generic/filter_bilateral.py
+++ anpr/generic/filter_bilateral.py
@@ -9,10 +9,10 @@ import numpy as np
 from anpr.core import ImageProcessor
 
 
-class FilterMean(ImageProcessor):
+class FilterBilateral(ImageProcessor):
     def __init__(self, kernel_size: int):
         self._kernel = kernel_size
-        
+
     def process(self, image: np.ndarray, sigma_space: int, sigma_color: int) -> np.ndarray:
         return cv2.bilateralFilter(src=image, 
                                    d=self._kernel, 
