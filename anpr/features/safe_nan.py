b594920022e44dce6417d33b27abe6a48e4a47b7
Moésio Filho <moesio.f@gmail.com>
2023-08-28 20:26:46 -0300

    feat(wip): entrega final

--- anpr/features/safe_nan.py
+++ anpr/features/safe_nan.py
@@ -8,20 +8,21 @@ import numpy as np
 from anpr.core.feature_extractor import FeatureExtractor
 
 
-class AggregateExtractor(FeatureExtractor):
+class SafeNanExtractor(FeatureExtractor):
     """Agregador de etapas de processamento
     sequencial. Aplica cada um dos processamentos
     de forma sequencial (i.e., saída de um é entrada
     para o próximo).
     """
 
-    def __init__(self, extractors: list[FeatureExtractor]) -> None:
-        self._extractors = extractors
+    def __init__(self, 
+                 extractor: FeatureExtractor,
+                 nan_value=0.0) -> None:
+        self._extractor = extractor
+        self._nan_value = nan_value
 
     def extract(self, image: np.ndarray) -> np.ndarray:
-        features = []
-
-        for p in self._extractors:
-            features.append(p.extract(image))
-
-        return np.concatenate(features)
+        feature = self._extractor.extract(image)
+        feature = np.nan_to_num(feature, 
+                                nan=self._nan_value)
+        return feature
