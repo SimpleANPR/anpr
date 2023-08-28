3b76af71001e784949b9e4cc89afc1bfcd46b9fb
Moésio Filho <moesio.f@gmail.com>
2023-08-28 20:49:11 -0300

    feat: update edge detector

--- anpr/detection/edge_detector.py
+++ anpr/detection/edge_detector.py
@@ -12,13 +12,17 @@ from anpr.generic.filter_bilateral import FilterBilateral
 from anpr.generic.canny import Canny
 from anpr.core.image_processor import ImageProcessor
 
+
 @dataclass(frozen=True)
 class DetectionExtras:
     candidates: list[tuple[int, int, int, int]]
     candidates_img: list[np.ndarray]
 
+
 class EdgeDetector(PlateDetector):
-    def __init__(self, low_pass: ImageProcessor = None, high_pass: ImageProcessor = None) -> None:
+    def __init__(self,
+                 low_pass: ImageProcessor = None,
+                 high_pass: ImageProcessor = None) -> None:
 
         if low_pass is None:
             low_pass = FilterBilateral(11, 17, 17)
@@ -31,14 +35,15 @@ class EdgeDetector(PlateDetector):
 
     def detect(self, image: np.ndarray) -> DetectionResult:
         original_image = image
-    
+
         if len(image.shape) == 3:
             image = GrayScale().process(image)
-        
+
         smoothed = self.low_pass.process(image)
         edges = self.high_pass.process(smoothed)
 
-        contours, _ = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(
+            edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
         location = None
@@ -50,22 +55,22 @@ class EdgeDetector(PlateDetector):
 
         if location is not None:
             mask = np.zeros(image.shape, np.uint8)
-            new_image = cv2.drawContours(mask, [location], 0,255, -1)
+            new_image = cv2.drawContours(mask, [location], 0, 255, -1)
             new_image = cv2.bitwise_and(image, image, mask=mask)
 
-            (x,y) = np.where(mask==255)
+            (x, y) = np.where(mask == 255)
             (x1, y1) = (np.min(x), np.min(y))
             (x2, y2) = (np.max(x), np.max(y))
-            cropped_image = image[x1:x2+1, y1:y2+1]
+            cropped_image = original_image[x1:x2+1, y1:y2+1]
 
             return DetectionResult(
                 found_plate=True,
                 n_candidates=len(contours),
-                plate_polygon=location,
+                plate_polygon=cv2.boundingRect(location),
                 plate_image=cropped_image)
         else:
             return DetectionResult(
                 found_plate=False,
-                n_candidates=len(contours), 
+                n_candidates=len(contours),
                 plate_polygon=None,
                 plate_image=None)
